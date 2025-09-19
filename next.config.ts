9ca3b5267dedfd1bca96b0204ac21e628a04c5dd
louisc <lcasine@my.wgu.edu>
2025-09-19 17:13:25 -0400

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  public: { formats: ['image/png'] },
+  
+  
 };
 
 export default nextConfig;
